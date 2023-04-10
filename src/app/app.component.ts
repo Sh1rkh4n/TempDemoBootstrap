fee343fbfd93e37e56d3be6c23f5135eb7d3640b
Adam Kovari <kvriady@gmail.com>
2023-04-10 20:20:42 +0200

    init(project)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TempDemo';
+
+  users = [
+    { id: 1, name: 'Jhon', isActive: true },
+    { id: 2, name: 'Wick', isActive: true },
+    { id: 3, name: 'Mike', isActive: true },
+  ];
+
+  users$ = of(this.users);
+  usernames$ = this.users$.pipe(map((users) => users.map((user) => user.name)));
+
+  ngOnInit(): void {
+    // this.users$.subscribe((users) => {
+    //   console.log('users', users);
+    // });
+  }
 }
