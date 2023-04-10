a1c36bf9dd1c6d9dd48eb74c3f20a8bf65273ea3
Adam Kovari <kvriady@gmail.com>
2023-04-10 21:05:52 +0200

    init(popoverjs)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddRestaurantComponent } from './add-restaurant/add-restaurant.component';
+import { EditRestaurantComponent } from './edit-restaurant/edit-restaurant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'add-restaurant', component: AddRestaurantComponent },
+  { path: 'edit-restaurant/:id', component: EditRestaurantComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { map, of } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  title = 'TempDemo';
-
-  users = [
-    { id: 1, name: 'Jhon', isActive: true },
-    { id: 2, name: 'Wick', isActive: true },
-    { id: 3, name: 'Mike', isActive: true },
-  ];
-
-  users$ = of(this.users);
-  usernames$ = this.users$.pipe(map((users) => users.map((user) => user.name)));
-
-  ngOnInit(): void {
-    // this.users$.subscribe((users) => {
-    //   console.log('users', users);
-    // });
-  }
+export class AppComponent {
+  title = 'TempDemoBootstrap';
 }
