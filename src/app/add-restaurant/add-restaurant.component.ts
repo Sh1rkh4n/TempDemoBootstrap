96b8a8b730573ca343a268447934e4e790f4dd1a
Adam Kovari <kvriady@gmail.com>
2023-04-11 19:23:37 +0200

    feat(working CSS and RIPPLE javascript)

--- src/app/add-restaurant/add-restaurant.component.ts
+++ src/app/add-restaurant/add-restaurant.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-add-restaurant',
   templateUrl: './add-restaurant.component.html',
-  styleUrls: ['./add-restaurant.component.css']
+  styleUrls: ['./add-restaurant.component.css'],
 })
 export class AddRestaurantComponent {
+  @ViewChild('button') button!: ElementRef;
 
+  onButtonClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const rect = target.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    target.classList.remove('clicked');
+    void target.offsetWidth;
+    target.classList.add('clicked');
+
+    target.querySelector('.ripple-effect:after')?.setAttribute('style', `top: ${y}px; left: ${x}px;`);
+  }
+
+  onClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const rect = target.getBoundingClientRect();
+    const x = event.clientX - rect.left; //this.button.nativeElement.offsetLeft;
+    const y = event.clientY - rect.top; //this.button.nativeElement.offsetTop;
+    const ripple = this.button.nativeElement.querySelector('.ripple-effect') as HTMLElement;
+
+    ripple.style.top = `${y}px`;
+    ripple.style.left = `${x}px`;
+    ripple.classList.add('clicked');
+    setTimeout(() => ripple.classList.remove('clicked'), 150);
+  }
 }
