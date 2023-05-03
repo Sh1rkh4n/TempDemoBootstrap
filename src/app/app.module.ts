0ac00b7cb04aa1d35d0ef1a0ba78d3cc782592d5
Adam Kovari <kvriady@gmail.com>
2023-05-03 20:59:54 +0200

    latest working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AddRestaurantComponent } from './add-restaurant/add-restaurant.componen
 import { EditRestaurantComponent } from './edit-restaurant/edit-restaurant.component';
 import { RemRestaurantComponent } from './rem-restaurant/rem-restaurant.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     RemRestaurantComponent,
     DashboardComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { RestaurantService } from '../restaurant.service';
+import { Restaurant } from '../restaurant';
+import { RemRestaurantComponent } from '../rem-restaurant/rem-restaurant.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements AfterViewInit, OnInit {
+  // oninit
+  displayedColumns: string[] = ['id', 'name', 'owner', 'mobile', 'email', 'location', 'rating', 'action'];
+  restaurants: Restaurant[] = [];
 
+  constructor(private restaurant: RestaurantService) {}
+  ngOnInit(): void {
+    this.restaurant.getAllRestaurant().subscribe((data) => {
+      this.restaurants = data;
+    });
+  }
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  // openDeleteModal(enterAnimationDuration: string, exitAnimationDuration: string, id: number, name: string): void {
+  //   const deleteConfirmConst = this.dialog.open(RemRestaurantComponent, {
+  //     width: '350px',
+  //     enterAnimationDuration,
+  //     exitAnimationDuration,
+  //     data: { id, name },
+  //   });
+
+  //   deleteConfirmConst.afterClosed().subscribe((result) => {
+  //     if (result) {
+  //       this.restaurants = this.restaurants.filter((f) => f.id !== id);
+  //     }
+  //   });
+  // }
 }
